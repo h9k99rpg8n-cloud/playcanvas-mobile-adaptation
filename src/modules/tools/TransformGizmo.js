f7af614dc6b23de32b4dd7452be2b5562bfd3f54
h9k99rpg8n-cloud <h9k99rpg8n@privaterelay.appleid.com>
2026-07-05 16:30:47 -0600

    Inline transform gizmo in tools module

--- src/modules/tools/TransformGizmo.js
+++ src/modules/tools/TransformGizmo.js
@@ -1,1 +1,28 @@
-export { TransformGizmo } from '../gizmos/TransformGizmo.js';
+import { TransformControls } from 'three/addons/controls/TransformControls.js';
+
+export class TransformGizmo {
+  constructor(camera, canvas, orbitController) {
+    this.control = new TransformControls(camera, canvas);
+    this.control.setMode('translate');
+    this.control.setSize(1.45);
+    this.control.visible = false;
+    this.control.addEventListener('dragging-changed', (event) => {
+      orbitController.setEnabled(!event.value);
+    });
+  }
+
+  attach(object) {
+    this.control.attach(object);
+    this.control.visible = true;
+  }
+
+  detach() {
+    this.control.detach();
+    this.control.visible = false;
+  }
+
+  setMode(mode) {
+    this.control.setMode(mode);
+    this.control.setSize(mode === 'rotate' ? 1.35 : 1.6);
+  }
+}
